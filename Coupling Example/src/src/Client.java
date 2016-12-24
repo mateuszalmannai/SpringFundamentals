0c14c50a8a82780c50668045449801414d3840d8
mateusz <mateuszalmannai@googlemail.com>
2016-12-24 20:16:54 +0100

    stop tracking HSQLdB output

--- Coupling Example/src/src/Client.java
+++ Coupling Example/src/src/Client.java
@@ -1,6 +1,7 @@
 package src;
 
 public class Client {
+  // make sure to delete leftover HSQL .script and .properties files when running other/new project
   public static void main(String[] args) {
 
     // 3. Centralise Configuration of the application
