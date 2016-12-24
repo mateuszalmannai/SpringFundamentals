2705cac64bdfc27478d7f71236ba2a73fdb8edcb
mateusz <mateuszalmannai@googlemail.com>
2016-12-24 17:55:34 +0100

    program to an interface, add dependency injection and a crude IoC container

--- Coupling Example/src/src/Client.java
+++ Coupling Example/src/src/Client.java
@@ -2,9 +2,12 @@ package src;
 
 public class Client {
   public static void main(String[] args) {
-    InvoiceService invoices = new InvoiceService();
 
-    Invoice newInvoice = new Invoice("11075", "Bill McLarane");
+    // 3. Centralise Configuration of the application
+    InvoiceService invoices = CrudeContainer.getInvoiceService();
+
+
+    Invoice newInvoice = new Invoice("11080", "Bill McGregor");
 
     invoices.raiseInvoice(newInvoice);
   }
